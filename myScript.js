745c29a94eca5082101de23816f97d6de4515ce9
daliduraku <mesherbetmolla@gmail.com>
2024-09-30 21:35:47 -0400

    added method to clear the previous nodes when a new grid is created

--- myScript.js
+++ myScript.js
@@ -1,20 +1,43 @@
-let color = "#D3D3D3"
+let clear = document.querySelector("button");
+let container = document.querySelector(".container"); 
 
-const cdiv = document.querySelector('.container');
-// add 16 divs
+clear.addEventListener('click', ()=>{ // we only want to add this listener once
+    resetSize()
+})
 
-for(let i = 0; i < 273; i++){
-    const div = document.createElement('div');
-    cdiv.appendChild(div);
 
-    div.addEventListener("mouseover", function(){
-        this.style.backgroundColor = color;
-    })
-
-    /*
-    div.addEventListener("mouseout", function(){
-
-        this.style.backgroundColor = "";
-    })
-    */
+function resetSize(){
+        
+        let number = prompt("What size would you like the grid to be? (1-100)");
+        createGrid(number); // call the createGrid function here and pass the number they entered as the argument. 
 }
+        
+        
+//function to create a 16x16 grid
+function createGrid(size){
+    
+    container.replaceChildren();
+    for (let i = 0; i < size*size; i++) {
+        
+        let square = document.createElement("div");
+        square.style.backgroundColor = "white";
+        container.appendChild(square);
+
+        //change background color of a square on hover
+        square.addEventListener('mouseover', function(){
+            square.style.backgroundColor = "black";
+        })
+
+        //function to reset the grid
+        function clearGrid(){
+            clear.addEventListener('click', e=>{
+                square.style.backgroundColor = "white"
+           })
+        }
+
+        clearGrid();    
+    }
+
+}
+
+createGrid(16);
