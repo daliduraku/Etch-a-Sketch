41916d98a4c90acbfb03f6c22eb8ba164a45b6a2
daliduraku <mesherbetmolla@gmail.com>
2024-09-30 22:50:20 -0400

    adjusting the width and height sizes when a new grid is created.

--- myScript.js
+++ myScript.js
@@ -9,7 +9,11 @@ clear.addEventListener('click', ()=>{ // we only want to add this listener once
 function resetSize(){
         
         let number = prompt("What size would you like the grid to be? (1-100)");
-        createGrid(number); // call the createGrid function here and pass the number they entered as the argument. 
+        if (number < 1 || number > 100 || isNaN(number)){
+            alert("Please enter a number between 1 and 100.");
+            return;
+        }
+        createGrid(number); 
 }
         
         
@@ -17,25 +21,22 @@ function resetSize(){
 function createGrid(size){
     
     container.replaceChildren();
+    
+    let squareSize = 900 / size;
+    
     for (let i = 0; i < size*size; i++) {
         
         let square = document.createElement("div");
         square.style.backgroundColor = "white";
+        square.style.width = `${squareSize}px`;  
+        square.style.height = `${squareSize}px`; 
         container.appendChild(square);
 
         //change background color of a square on hover
         square.addEventListener('mouseover', function(){
             square.style.backgroundColor = "black";
-        })
-
-        //function to reset the grid
-        function clearGrid(){
-            clear.addEventListener('click', e=>{
-                square.style.backgroundColor = "white"
-           })
-        }
-
-        clearGrid();    
+        });
+   
     }
 
 }
